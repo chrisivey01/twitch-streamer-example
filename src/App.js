29a7b7f5185e973a0bbad5e9499646aab4cb79af
chris <chrisivey01@gmail.com>
2020-05-26 15:43:16 -0500

    update example

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import TwitchClient from 'twitch';
 
 function App() {
+
+  let accessToken, getUserId;
+  const clientId = "i9hswbac2b2s2n5qzxdq0gtmsepdwu"
+  // const clientSecret = "uog8134agr1kbxq3nfge0d9o896ac1"
+  const fetchData = () => {
+
+    axios.post('https://id.twitch.tv/oauth2/token?client_id='+clientId+'&client_secret='+clientSecret+'&grant_type=client_credentials').then(results =>{
+      accessToken = results.data.access_token
+      console.log(accessToken)
+    })
+  }
+
+  const checkStreamer = async() => {
+    const client = TwitchClient.withCredentials(clientId, accessToken);
+    const user = await client.helix.users.getUserByName('chrisivey01');
+    getUserId = user._data.id
+    console.log(getUserId)
+    
+  }
+
+  const loadStreamers = async() => {
+    const client = TwitchClient.withCredentials(clientId, accessToken);
+    const stream = await client.kraken.streams.getStreamByChannel(getUserId);
+    console.log(stream)
+
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={fetchData}>Fetch me!</button>
+
+      <button onClick={checkStreamer}> Check Streamer </button>
+
+      <button onClick={loadStreamers}> Load streamers </button>
     </div>
   );
 }
